82e56ef29fd3ba9722c88b74096d3a3f0a162e81
Nostro <49858211+Nostrorom@users.noreply.github.com>
2022-03-06 16:22:50 +0100

    adapter-netlify

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -7,13 +7,12 @@ const dev = mode === 'development';
 
 const config = {
 	plugins: [
-		//Some plugins, like tailwindcss/nesting, need to run before Tailwind,
-		//But others, like autoprefixer, need to run after,
-		tailwindcss(), //Some plugins, like tailwindcss/nesting, need to run before Tailwind, tailwindcss(), //But others, like autoprefixer, need to run after, autoprefixer(), !dev &&
-		cssnano({
-			preset: 'default'
-		}),
-		autoprefixer
+		tailwindcss(),
+		autoprefixer(),
+		!dev &&
+			cssnano({
+				preset: 'default'
+			})
 	]
 };
 
