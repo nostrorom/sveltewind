af559484e29f9b65824882c93b758c5f0c9d750c
Nostro <49858211+Nostrorom@users.noreply.github.com>
2022-03-06 14:14:32 +0100

    tailwind v3

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,39 +1,36 @@
 const colors = require('tailwindcss/colors');
 
 const config = {
-	mode: "jit",
-	purge: [
-		"./src/**/*.{html,js,svelte,ts}",
-	],
+	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
 		extend: {},
-        colors: {
-            white: colors.white,
-            black: colors.black,
-            red: colors.red,
-            orange: colors.orange,
-            amber: colors.amber,
-            yellow: colors.yellow,
-            green: colors.green,
-            lime: colors.lime,
-            emerald: colors.emerald,
-            teal: colors.teal,
-            cyan: colors.cyan,
-            skyblue: colors.sky,      
-            blue: colors.blue,
-            indigo: colors.indigo,
-            violet: colors.violet,
-            purple: colors.purple,
-            fuchsia: colors.fuchsia,
-            pink: colors.pink,
-            rose: colors.rose,
-            blueGray: colors.blueGray,
-            coolGray: colors.coolGray,
-            gray: colors.gray,
-            trueGray: colors.trueGray,
-            warmGray: colors.warmGray,
-        },
+		colors: {
+			white: colors.white,
+			black: colors.black,
+			red: colors.red,
+			orange: colors.orange,
+			amber: colors.amber,
+			yellow: colors.yellow,
+			green: colors.green,
+			lime: colors.lime,
+			emerald: colors.emerald,
+			teal: colors.teal,
+			cyan: colors.cyan,
+			skyblue: colors.sky,
+			blue: colors.blue,
+			indigo: colors.indigo,
+			violet: colors.violet,
+			purple: colors.purple,
+			fuchsia: colors.fuchsia,
+			pink: colors.pink,
+			rose: colors.rose,
+			slate: colors.slate,
+			zinc: colors.zinc,
+			gray: colors.gray,
+			stone: colors.stone,
+			neutral: colors.neutral
+		}
 	},
-	plugins: [],
+	plugins: []
 };
 module.exports = config;
