a5b85a06bd45f5299bf6f71747eb7f3cf10064ba
Nostro <49858211+Nostrorom@users.noreply.github.com>
2022-03-06 14:36:09 +0100

    adapter-netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,20 @@
 import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
+
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
+	kit: {
+		adapter: adapter({ split: false }),
+		vite: {
+			target: '#svelte'
+		}
+	},
 	preprocess: [
 		preprocess({
 			postcss: true
 		})
-	],
-
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
+	]
 };
 
 export default config;
