eb84b820bdfaf443f5d624213003251bf2ffa7da
Nostro <49858211+Nostrorom@users.noreply.github.com>
2022-06-22 20:43:37 +0200

    update packages && playwirght config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,10 +1,28 @@
-import type { PlaywrightTestConfig } from '@playwright/test';
+import { type PlaywrightTestConfig, devices } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
 	webServer: {
 		command: 'npm run build && npm run preview',
 		port: 3000
-	}
+	},
+	forbidOnly: !!process.env.CI,
+	retries: process.env.CI ? 2 : 0,
+	use: {
+		trace: 'on-first-retry'
+	},
+	projects: [
+		{
+			name: 'chromium',
+			use: { ...devices['Desktop Chrome'] }
+		},
+		{
+			name: 'firefox',
+			use: { ...devices['Desktop Firefox'] }
+		},
+		{
+			name: 'webkit',
+			use: { ...devices['Desktop Safari'] }
+		}
+	]
 };
-
 export default config;
