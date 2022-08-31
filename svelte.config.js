06080b0c7ec30132d80a93c1e13e5543f0ee72af
Nostro <49858211+Nostrorom@users.noreply.github.com>
2022-08-31 19:40:22 +0200

    package update

--- svelte.config.js
+++ svelte.config.js
@@ -3,10 +3,18 @@ import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: preprocess(),
+	preprocess: [
+		preprocess({
+			postcss: true
+		})
+	],
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		alias: {
+			$types: '/src/types',
+			$shared: '/src/shared'
+		}
 	}
 };
 
