497f1a100edb67c017287264b42b4a1f7f878a92
nostro <49858211+Nostrorom@users.noreply.github.com>
2023-06-21 21:31:59 +0200

    bump

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,29 +1,12 @@
-import { type PlaywrightTestConfig, devices } from '@playwright/test';
+import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
 	webServer: {
 		command: 'npm run build && npm run preview',
 		port: 4173
 	},
-	forbidOnly: !!process.env.CI,
-	retries: process.env.CI ? 2 : 0,
-	use: {
-		trace: 'on-first-retry'
-	},
-	projects: [
-		{
-			name: 'chromium',
-			use: { ...devices['Desktop Chrome'] }
-		},
-		{
-			name: 'firefox',
-			use: { ...devices['Desktop Firefox'] }
-		},
-		{
-			name: 'webkit',
-			use: { ...devices['Desktop Safari'] }
-		}
-	]
+	testDir: 'tests',
+	testMatch: /(.+\.)?(test|spec)\.[jt]s/
 };
 
 export default config;
--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -14,18 +14,18 @@
 </script>
 
 <header
-	class="flex h-full w-full justify-between items-center px-4 py-2 bg-neutral-200 dark:bg-neutral-800"
+	class="flex items-center justify-between w-full h-full px-4 py-2 bg-neutral-200 dark:bg-neutral-800"
 >
 	<div>Svelte</div>
 	<nav>
 		<ul class="flex h-full space-x-4">
 			<li class="h-full hover:text-amber-500">
-				<a class="" data-sveltekit-prefetch href="/">
+				<a class="" data-sveltekit-preload-data="hover" href="/">
 					<div class="" class:text-emerald-600={$page.url.pathname === '/'}>Home</div>
 				</a>
 			</li>
 			<li class="h-full hover:text-amber-500">
-				<a class="" data-sveltekit-prefetch href="/about">
+				<a class="" data-sveltekit-preload-data="hover" href="/about">
 					<div class="" class:text-fuchsia-600={$page.url.pathname === '/about'}>About</div>
 				</a>
 			</li>
